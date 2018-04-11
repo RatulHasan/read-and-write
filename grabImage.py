b323b635dad3f6afaa2507e626f98a2fa94972df
Ratul Hasan <ratuljh@gmail.com>
2018-04-11 16:54:02 +0600

    grab html and image

--- grabImage.py
+++ grabImage.py
@@ -1,3 +1,20 @@
 import requests
 from io import BytesIO
 from PIL import Image
+
+# r = requests.get("https://images8.alphacoders.com/815/815404.jpg")
+r = requests.get("https://i.pinimg.com/originals/d3/f0/eb/d3f0ebdf9705f2b9eefdcb0d7df99b76.jpg")
+
+print("Status", r.status_code)
+
+image = Image.open(BytesIO(r.content))
+
+print(image.size, image.format, image.mode)
+
+path = "./image." + image.format
+
+try:
+    image.save(path, image.format)
+
+except IOError:
+    print("Can\'t save image!")
